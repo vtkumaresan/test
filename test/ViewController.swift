ed4b33f182b08c03ee119caa6043d19462dbd30e
John <testcseapi@gmail.com>
2014-11-11 16:44:01 +0530

    message 22

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        NSLog("message2");
+        NSLog("message22");
     }
 
     override func didReceiveMemoryWarning() {
